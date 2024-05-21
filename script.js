7fee218c9e5a09b4e1198d68770c39e126c50a63
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2024-05-21 20:54:23 +0500

    add a change

---
+++ script.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.about-us h1, .order h1, .contant .h1', {origin: 'top'})
+ScrollReveal().reveal('.about-us', {origin: 'rightp'})
+ScrollReveal().reveal('.about-img, .contact form', {origin: 'left'})
+ScrollReveal().reveal('.order .box, nav h1 ', {origin: 'bottom'})
